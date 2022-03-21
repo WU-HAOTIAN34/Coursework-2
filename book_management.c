3f27b60715ac09412702d63ce3393a296290d911
WuHaoTian12 <101766788+WuHaoTian12@users.noreply.github.com>
2022-03-21 15:25:30 +0800

    version 2

--- book_management.c
+++ book_management.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 
-static Book* library;
+
 
 int store_books(FILE* file) {
 	if (file == NULL) {
@@ -21,71 +21,98 @@ int load_books(FILE* file) {
 
 
 int add_book(Book book) {
-	Book* newBook= (Book*)malloc(sizeof(Book));
-	newBook->next = book.next;
-	book.next->before = newBook;
-	book.next = newBook;
-	newBook->before = book.before->next;
-	
-
-
+	Book* list = &library;
+	Book* newBook = &book;
+	int ID = 0;
+	do {
+		list = list->next;
+		ID++;
+	} while (list->next != NULL);
+	list->next = newBook;
+	newBook->id = ID;
+	newBook->next = NULL;
+	return 1;
 }
 
 
 
 int remove_book(Book book) {
-	if (book.before != NULL && book.next != NULL) {
-		book.before->next = book.next;
-		book.next->before = book.before;
-	}
-	else if (book.before == NULL && book.next == NULL) {
-		
-	}
-	else if (book.before == NULL){
-		
-	}
-	else if (book.next == NULL){
-		
-	}
-	Book* delBook= &book;
-	free(delBook);
+	int ID = 0;
+	Book* list = &library;
+	do {
+
+	} while (list->next->next != NULL);
 	return 0;
 }
 
 
 BookList find_book_by_title(const char* title) {
 	Book* query = library->next;
-	int num = 1;
-	while (query->next != NULL) {
-		if (strcmp(query->title, title)) {
-			BookList res = { &query, num };
-			return res;
-		}
-		else {
-			query = query->next;
-			num++;
-		}
+	Book* newBook;
+	int num = 0;
+	char bookTitle[30];
+	printf("Please enter the title you want to find: ");
+	scanf("%s", bookTitle);
+	BookList* res = (BookList*)malloc(sizeof(BookList));
+	
+	if (library->next == NULL) {
+		res->list = NULL;
+		res->length = 0;
 	}
-	BookList res = { NULL, 0 };
-	return res;
+	else {
+		
+		do {
+			query = query->next;
+			if (strcmp(query->title, title)) {
+				num++;
+				newBook = (Book*)malloc(sizeof(Book));
+				res->list = newBook;
+				newBook->id = num;
+				newBook->copies = query->copies;
+				newBook->year = query->year;
+				strcpy(newBook->authors, query->authors);
+				strcpy(newBook->title, query->title);
+				newBook->next = NULL;
+
+			}
+		} while (query->next != NULL);
+	}
+	return *res;
 }
 
 
 BookList find_book_by_author(const char* author) {
 	Book* query = library->next;
-	int num = 1;
-	while (query->next != NULL) {
-		if (strcmp(query->authors, author)) {
-			BookList res = { &query, num };
-			return res;
-		}
-		else {
-			query = query->next;
-			num++;
-		}
+	Book* newBook;
+	int num = 0;
+	char bookTitle[30];
+	printf("Please enter the title you want to find: ");
+	scanf("%s", bookTitle);
+	BookList* res = (BookList*)malloc(sizeof(BookList));
+
+	if (library->next == NULL) {
+		res->list = NULL;
+		res->length = 0;
 	}
-	BookList res = { NULL, 0 };
-	return res;
+	else {
+
+		do {
+			query = query->next;
+			if (strcmp(query->title, title)) {
+				num++;
+				newBook = (Book*)malloc(sizeof(Book));
+				res->list = newBook;
+				newBook->id = num;
+				newBook->copies = query->copies;
+				newBook->year = query->year;
+				strcpy(newBook->authors, query->authors);
+				strcpy(newBook->title, query->title);
+				newBook->next = NULL;
+
+			}
+		} while (query->next != NULL);
+	}
+	return *res;
 
 }
 
@@ -93,19 +120,36 @@ BookList find_book_by_author(const char* author) {
 
 BookList find_book_by_year(unsigned int year) {
 	Book* query = library->next;
-	int num = 1;
-	while (query->next != NULL) {
-		if (query->year == year) {
-			BookList res = { &query, num };
-			return res;
-		}
-		else {
-			query = query->next;
-			num++;
-		}
+	Book* newBook;
+	int num = 0;
+	char bookTitle[30];
+	printf("Please enter the title you want to find: ");
+	scanf("%s", bookTitle);
+	BookList* res = (BookList*)malloc(sizeof(BookList));
+
+	if (library->next == NULL) {
+		res->list = NULL;
+		res->length = 0;
 	}
-	BookList res = { NULL, 0 };
-	return res;
+	else {
+
+		do {
+			query = query->next;
+			if (strcmp(query->title, title)) {
+				num++;
+				newBook = (Book*)malloc(sizeof(Book));
+				res->list = newBook;
+				newBook->id = num;
+				newBook->copies = query->copies;
+				newBook->year = query->year;
+				strcpy(newBook->authors, query->authors);
+				strcpy(newBook->title, query->title);
+				newBook->next = NULL;
+
+			}
+		} while (query->next != NULL);
+	}
+	return *res;
 
 }
 
--- main.c
+++ main.c
@@ -1,38 +1,111 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 #include <malloc.h>
 #include "book_management.h"
 
 
-void signModule() {
-	char ID[10];
-	char password[8];
-	char* a = &ID[0];
-	/*int ID, password;*/
-	int i;
-	printf("Please enter your ID number(8-digit-number): ");
-	scanf("%s", a);
-	/*while()
-	scanf("%c%c%c%c%c%c%c%c", a, a + 1, a + 2, a + 3, a + 4, a + 5, a + 6, a + 7, a + 8);
-	for (i = 0; i < 8; i++) {
-		if ((int)ID[i] < 48 || (int)ID[i] >57) {
-			printf("adadad");
+
+
+
+
+void registerModule(user* a) {
+	user* list = a->next;
+	user* newUser;
+	char ID[40];
+	char password[40];
+	while (1) {
+		printf("Please enter your ID number(8-digit-number): ");
+		scanf("%s", ID);
+		if (strspn(ID, "0123456789") == strlen(ID) && strlen(ID) == 8) {
 			break;
 		}
-	}*/
-	
+		else {
+			printf("Format error, please enter again!\n");
+		}
+		while (list->next != NULL) {
+			if (strcmp(list->ID, ID)) {
+				printf("The ID already exists.");
+				break;
+			}
+		}
+	}
+	printf("Ok\n");
+	while (1) {
+		printf("Please enter your password(8-digit-number): ");
+		scanf("%s", password);
+		if (strspn(password, "0123456789") == strlen(password) && strlen(password) == 8) {
+			printf("Ok\n");
+			break;
+		}
+		else {
+			printf("Format error, please enter again!\n");
+		}
+	}
+	while (list->next != NULL) {
+		list = list->next;
+	}
+	newUser = (user*)malloc(sizeof(user));
+	list->next = newUser;
+	strcpy(newUser->ID, ID);
+	strcpy(newUser->password, password);
+	newUser->broBook = NULL;
+	newUser->next = NULL;
+	printf("Congratuulations. You have registered successfully!\n");
 
-	/*printf("Please enter your password(8-digit-number): ");
-	scanf("%d", &password);*/
 
+
+}
+
+void signModule(user* a) {
+	user* list = a->next;
+	user* newUser;
+	char enterID[40];
+	char enterpassword[40];       // Receive the entered ID and password
+	char password[9];
+	while (1) {
+		printf("Please enter your ID number(8-digit-number): ");
+		scanf("%s", enterID);
+		if (strspn(enterID, "0123456789") == strlen(enterID) && strlen(enterID) == 8) {
+			break;
+		}
+		else {
+			printf("Format error, please enter again!\n");
+		}
+		while (list->next != NULL) {
+			if (strcmp(list->ID, enterID)) {
+				strcmp(password, list->password);
+				break;
+			}
+		}
+		printf("The ID doesn't exist.");
+	}
+	while (1) {
+		printf("Please enter your password(8-digit-number): ");
+		scanf("%s", password);
+		if (strspn(password, "0123456789") == strlen(password) && strlen(password) == 8) {
+			printf("Ok\n");
+			break;
+		}
+		else {
+			printf("Format error, please enter again!\n");
+		}
+		if (strcmp(password, enterpassword)) {			
+			return;
+		}
+		else {
+			printf("Icorrect password, please enter again.");
+		}
+	}
 }
 
 
 int main() {
-	/*signModule();*/
-	char a[12] = "edadeadadagg";
+	signModule();
+	library = (Book*)malloc(sizeof(Book));
+	/*char a[12] = "edadeadadagg";
 	int i;
-	i = strspn(a, "abcdefghijklmnop\0");
-	printf("%d", i);
+	i = strspn("ababababababab", "abcdefghijklmnop\0");
+	printf("%d", i);*/
 	return 0;
 }

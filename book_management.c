772779c389be73fdd44cea3698035f85c0afd49c
WuHaoTian12 <101766788+WuHaoTian12@users.noreply.github.com>
2022-03-26 13:28:36 +0800

    version 5

--- book_management.c
+++ book_management.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "book_management.h"
 #include <malloc.h>
 #include <stdio.h>
@@ -9,26 +10,26 @@
 int store_books(FILE* file) {
 	int len;
 	Book* newBook = library->list;
-	fwrite(&(library->length), sizeof(int), 1, file);
+	fwrite(&library->length, sizeof(unsigned int), 1, file);
 	while (newBook != NULL) {
 		fwrite(&(newBook->id), sizeof(unsigned int), 1, file);
 		len = strlen(newBook->title);
 		fwrite(&(len), sizeof(int), 1, file);
 		fwrite(newBook->title, sizeof(char), len, file);
-		fwrite(&(len), sizeof(int), 1, file);
 		len = strlen(newBook->authors);
+		fwrite(&(len), sizeof(int), 1, file);
 		fwrite(newBook->authors, sizeof(char), len, file);
-		fwrite(newBook->year, sizeof(unsigned int), 2, file);
+		fwrite(&(newBook->year), sizeof(unsigned int), 2, file);
 		newBook = newBook->next;
 	}
 }
 
 
+
 int load_books(FILE* file) {
 	int len, i;
 	Book* newBook;
-	library = (BookList*)malloc(sizeof(BookList));
-	fread(&(library->length), sizeof(int), 1, file);
+	fread(&(library->length), sizeof(unsigned int), 1, file);
 	library->list = (Book*)malloc(sizeof(Book));
 	newBook = library->list;
 	for (i = 0; i < library->length; i++) {
@@ -42,141 +43,141 @@ int load_books(FILE* file) {
 		memset(newBook->authors, '\0', len + 1);
 		fread(newBook->authors, sizeof(char), len, file);
 		fread(&(newBook->year), sizeof(unsigned int), 2, file);
+		if (i == library->length - 1) {
+			newBook->next = NULL;
+			break;
+		}
+		else {
+			newBook->next = (Book*)malloc(sizeof(Book));
+			newBook = newBook->next;
+		}
 	}
 }
 
 
 
-int add_book(Book book) {
-	Book* list = &library;
-	Book* newBook = &book;
-	int ID = 0;
-	do {
-		list = list->next;
-		ID++;
-	} while (list->next != NULL);
-	list->next = newBook;
-	newBook->id = ID;
-	newBook->next = NULL;
-	return 1;
-}
-
-
-
-int remove_book(Book book) {
-	int ID = 0;
-	Book* list = &library;
-	do {
-
-	} while (list->next->next != NULL);
-	return 0;
-}
+//int add_book(Book book) {
+//	Book* query = library->list;
+//	while (query->next != NULL) {
+//		query = query->next;
+//	}
+//	query->next = (Book*)malloc(sizeof(Book));
+//
+//}
+//
+//
+//
+//int remove_book(Book book) {
+//	int ID = 0;
+//	Book* list = &library;
+//	do {
+//
+//	} while (list->next->next != NULL);
+//	return 0;
+//}
+//
 
 
 BookList find_book_by_title(const char* title) {
-	Book* query = library->next;
-	Book* newBook;
+	int i, len;
 	int num = 0;
-	char bookTitle[30];
-	printf("Please enter the title you want to find: ");
-	scanf("%s", bookTitle);
 	BookList* res = (BookList*)malloc(sizeof(BookList));
-	
-	if (library->next == NULL) {
-		res->list = NULL;
-		res->length = 0;
+	res->list = (Book*)malloc(sizeof(Book));
+	Book* newBook = res->list;
+	newBook->next = NULL;
+	res->list = NULL;
+	Book* query = library->list;
+	while (query != NULL) {
+		if (strcmp(query->title, title)) {
+			newBook->id = query->id;
+			len = strlen(query->title);
+			newBook->title = (char*)malloc(sizeof(len + 1));
+			memset(newBook->title, '\0', len + 1);
+			strcpy(newBook->title, query->title);
+			len = strlen(query->authors);
+			newBook->authors = (char*)malloc(sizeof(len + 1));
+			memset(newBook->authors, '\0', len + 1);
+			strcpy(newBook->authors, query->authors);
+			newBook->year = query->year;
+			newBook->copies = query->copies;
+			num++;
+			newBook->next = (Book*)malloc(sizeof(Book));
+			newBook = newBook->next;
+			newBook->next = NULL;
+		}
+		query = query->next;
 	}
-	else {
-		
-		do {
-			query = query->next;
-			if (strcmp(query->title, title)) {
-				num++;
-				newBook = (Book*)malloc(sizeof(Book));
-				res->list = newBook;
-				newBook->id = num;
-				newBook->copies = query->copies;
-				newBook->year = query->year;
-				strcpy(newBook->authors, query->authors);
-				strcpy(newBook->title, query->title);
-				newBook->next = NULL;
-
-			}
-		} while (query->next != NULL);
-	}
+	res->length = num;
 	return *res;
 }
 
 
+
 BookList find_book_by_author(const char* author) {
-	Book* query = library->next;
-	Book* newBook;
+	int i, len;
 	int num = 0;
-	char bookTitle[30];
-	printf("Please enter the title you want to find: ");
-	scanf("%s", bookTitle);
 	BookList* res = (BookList*)malloc(sizeof(BookList));
-
-	if (library->next == NULL) {
-		res->list = NULL;
-		res->length = 0;
+	res->list = (Book*)malloc(sizeof(Book));
+	Book* newBook = res->list;
+	newBook->next = NULL;
+	res->list = NULL;
+	Book* query = library->list;
+	while (query != NULL) {
+		if (strcmp(query->authors, author)) {
+			newBook->id = query->id;
+			len = strlen(query->title);
+			newBook->title = (char*)malloc(sizeof(len + 1));
+			memset(newBook->title, '\0', len + 1);
+			strcpy(newBook->title, query->title);
+			len = strlen(query->authors);
+			newBook->authors = (char*)malloc(sizeof(len + 1));
+			memset(newBook->authors, '\0', len + 1);
+			strcpy(newBook->authors, query->authors);
+			newBook->year = query->year;
+			newBook->copies = query->copies;
+			num++;
+			newBook->next = (Book*)malloc(sizeof(Book));
+			newBook = newBook->next;
+			newBook->next = NULL;
+		}
+		query = query->next;
 	}
-	else {
-
-		do {
-			query = query->next;
-			if (strcmp(query->title, title)) {
-				num++;
-				newBook = (Book*)malloc(sizeof(Book));
-				res->list = newBook;
-				newBook->id = num;
-				newBook->copies = query->copies;
-				newBook->year = query->year;
-				strcpy(newBook->authors, query->authors);
-				strcpy(newBook->title, query->title);
-				newBook->next = NULL;
-
-			}
-		} while (query->next != NULL);
-	}
+	res->length = num;
 	return *res;
-
 }
 
 
 
 BookList find_book_by_year(unsigned int year) {
-	Book* query = library->next;
-	Book* newBook;
+	int i, len;
 	int num = 0;
-	char bookTitle[30];
-	printf("Please enter the title you want to find: ");
-	scanf("%s", bookTitle);
 	BookList* res = (BookList*)malloc(sizeof(BookList));
-
-	if (library->next == NULL) {
-		res->list = NULL;
-		res->length = 0;
+	res->list = (Book*)malloc(sizeof(Book));
+	Book* newBook = res->list;
+	newBook->next = NULL;
+	res->list = NULL;
+	Book* query = library->list;
+	while (query != NULL) {
+		if (query->year == year) {
+			newBook->id = query->id;
+			len = strlen(query->title);
+			newBook->title = (char*)malloc(sizeof(len + 1));
+			memset(newBook->title, '\0', len + 1);
+			strcpy(newBook->title, query->title);
+			len = strlen(query->authors);
+			newBook->authors = (char*)malloc(sizeof(len + 1));
+			memset(newBook->authors, '\0', len + 1);
+			strcpy(newBook->authors, query->authors);
+			newBook->year = query->year;
+			newBook->copies = query->copies;
+			num++;
+			newBook->next = (Book*)malloc(sizeof(Book));
+			newBook = newBook->next;
+			newBook->next = NULL;
+		}
+		query = query->next;
 	}
-	else {
-
-		do {
-			query = query->next;
-			if (strcmp(query->title, title)) {
-				num++;
-				newBook = (Book*)malloc(sizeof(Book));
-				res->list = newBook;
-				newBook->id = num;
-				newBook->copies = query->copies;
-				newBook->year = query->year;
-				strcpy(newBook->authors, query->authors);
-				strcpy(newBook->title, query->title);
-				newBook->next = NULL;
-
-			}
-		} while (query->next != NULL);
-	}
+	res->length = num;
 	return *res;
-
 }
 
--- main.c
+++ main.c
@@ -33,6 +33,7 @@ void registerModule(user* a) {
 	printf("Ok\n");
 	while (1) {
 		printf("Please enter your password(8-digit-number): ");
+		memset(password, '\0', sizeof(password));
 		scanf("%s", password);
 		if (strspn(password, "0123456789") == strlen(password) && strlen(password) == 8) {
 			printf("Ok\n");
@@ -101,11 +102,179 @@ void signModule(user* a) {
 
 
 int main() {
-	signModule();
-	library = (Book*)malloc(sizeof(Book));
+	/*signModule();
+	library = (Book*)malloc(sizeof(Book));*/
 	/*char a[12] = "edadeadadagg";
 	int i;
 	i = strspn("ababababababab", "abcdefghijklmnop\0");
 	printf("%d", i);*/
+
+
+	/*int d;
+	int a = 100;
+	FILE* b= fopen("a.txt", "wb+");
+	fwrite(&a, sizeof(int), 1, b);
+	fclose(b);
+	FILE* c = fopen("a.txt", "rb");
+	fread(&d, sizeof(int), 1, c);
+	fclose(c);
+	printf("%d", d);*/
+
+
+
+
+
+	char a[100];
+	memset(a, '\0', sizeof(char) * 100);
+	library = (BookList*)malloc(sizeof(BookList));
+	
+	FILE* B = fopen("book.txt", "rb");
+	load_books(B);
+	fclose(B);
+
+	
+	/*Book* newBook;
+	library->list = (Book*)malloc(sizeof(Book));
+	newBook = library->list;
+	
+	library->length = 7;
+
+	newBook->id = 1;
+	strcpy(a, "Frank Herbert");
+	newBook->title = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->title, '\0', strlen(a) + 1);
+	strcpy(newBook->title, a);
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "Dune");
+	newBook->authors = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->authors, '\0', strlen(a) + 1);
+	strcpy(newBook->authors, a);
+	newBook->year = 1978;
+	newBook->copies = 23;
+	printf("%s\n", newBook->title);
+	printf("%s\n", newBook->authors);
+	newBook->next = (Book*)malloc(sizeof(Book));
+	newBook = newBook->next;
+
+
+	newBook->id = 2;
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "Isaac Asimov");
+	newBook->title = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->title, '\0', strlen(a) + 1);
+	strcpy(newBook->title, a);
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "I,Robot");
+	newBook->authors = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->authors, '\0', strlen(a) + 1);
+	strcpy(newBook->authors, a);
+	newBook->year = 2013;
+	newBook->copies = 102;
+	printf("%s\n", newBook->title);
+	printf("%s\n", newBook->authors);
+	newBook->next = (Book*)malloc(sizeof(Book));
+	newBook = newBook->next;
+	
+	newBook->id = 3;
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "William Gibson");
+	newBook->title = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->title, '\0', strlen(a) + 1);
+	strcpy(newBook->title, a);
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "Neuromancer");
+	newBook->authors = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->authors, '\0', strlen(a) + 1);
+	strcpy(newBook->authors, a);
+	newBook->year = 1846;
+	newBook->copies = 42;
+	printf("%s\n", newBook->title);
+	printf("%s\n", newBook->authors);
+	newBook->next = (Book*)malloc(sizeof(Book));
+	newBook = newBook->next;
+	
+	newBook->id = 4;
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "Ray Bradbury");
+	newBook->title = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->title, '\0', strlen(a) + 1);
+	strcpy(newBook->title, a);
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "Farenheit 451");
+	newBook->authors = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->authors, '\0', strlen(a) + 1);
+	strcpy(newBook->authors, a);
+	newBook->year = 1685;
+	newBook->copies = 345;
+	printf("%s\n", newBook->title);
+	printf("%s\n", newBook->authors);
+	newBook->next = (Book*)malloc(sizeof(Book));
+	newBook = newBook->next;
+	
+	newBook->id = 5;
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "Andy Weir");
+	newBook->title = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->title, '\0', strlen(a) + 1);
+	strcpy(newBook->title, a);
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "The Martian");
+	newBook->authors = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->authors, '\0', strlen(a) + 1);
+	strcpy(newBook->authors, a);
+	newBook->year = 1959;
+	newBook->copies = 13;
+	printf("%s\n", newBook->title);
+	printf("%s\n", newBook->authors);
+	newBook->next = (Book*)malloc(sizeof(Book));
+	newBook = newBook->next;
+
+	newBook->id = 6;
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "Neal Stephenson");
+	newBook->title = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->title, '\0', strlen(a) + 1);
+	strcpy(newBook->title, a);
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "Snow Crash");
+	newBook->authors = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->authors, '\0', strlen(a) + 1);
+	strcpy(newBook->authors, a);
+	newBook->year = 1586;
+	newBook->copies = 79;
+	printf("%s\n", newBook->title);
+	printf("%s\n", newBook->authors);
+	newBook->next = (Book*)malloc(sizeof(Book));
+	newBook = newBook->next;
+
+	newBook->id = 7;
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "Jeff Noon");
+	newBook->title = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->title, '\0', strlen(a) + 1);
+	strcpy(newBook->title, a);
+	memset(a, '\0', sizeof(char) * 100);
+	strcpy(a, "Vurt");
+	newBook->authors = (char*)malloc(sizeof(char) * (strlen(a) + 1));
+	memset(newBook->authors, '\0', strlen(a) + 1);
+	strcpy(newBook->authors, a);
+	newBook->year = 1999;
+	newBook->copies = 156;
+	printf("%s\n", newBook->title);
+	printf("%s\n", newBook->authors);
+	newBook->next = NULL;
+
+	
+	
+
+
+
+	FILE* b = fopen("book.txt", "wb+");
+	store_books(b);
+	fclose(b);*/
+
+
+
+	printf("Ok");
 	return 0;
 }
